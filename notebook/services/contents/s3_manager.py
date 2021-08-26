a5263c77d60b3ef41cd37e01d05f54944f668b55
Ahmed Sharief R <ahmedshariefr@rempc1hp1jz.ads.autodesk.com>
2021-08-26 07:53:22 +0530

    bug fix for s3 rename and delete files

--- notebook/services/contents/s3_manager.py
+++ notebook/services/contents/s3_manager.py
@@ -3,6 +3,57 @@ import boto3
 
 s3_client = boto3.client('s3')
 
+def get_matching_s3_objects(bucket, prefix='', suffix=''):
+    """
+    Generate objects in an S3 bucket.
+    :param bucket: Name of the S3 bucket.
+    :param prefix: Only fetch objects whose key starts with
+        this prefix (optional).
+    :param suffix: Only fetch objects whose keys end with
+        this suffix (optional).
+    """
+    kwargs = {'Bucket': bucket}
+
+    # If the prefix is a single string (not a tuple of strings), we can
+    # do the filtering directly in the S3 API.
+    if isinstance(prefix, str):
+        kwargs['Prefix'] = prefix
+
+    while True:
+
+        # The S3 API response is a large blob of metadata.
+        # 'Contents' contains information about the listed objects.
+        resp = s3_client.list_objects_v2(**kwargs)
+
+        try:
+            contents = resp['Contents']
+        except KeyError:
+            return
+
+        for obj in contents:
+            key = obj['Key']
+            if key.startswith(prefix) and key.endswith(suffix):
+                yield obj
+
+        # The S3 API is paginated, returning up to 1000 keys at a time.
+        # Pass the continuation token into the next response, until we
+        # reach the final page (when this field is missing).
+        try:
+            kwargs['ContinuationToken'] = resp['NextContinuationToken']
+        except KeyError:
+            break
+
+
+def get_matching_s3_keys(bucket, prefix='', suffix=''):
+    """
+    Generate the keys in an S3 bucket.
+    :param bucket: Name of the S3 bucket.
+    :param prefix: Only fetch keys that start with this prefix (optional).
+    :param suffix: Only fetch keys that end with this suffix (optional).
+    """
+    for obj in get_matching_s3_objects(bucket, prefix, suffix):
+        yield obj['Key']
+
 class S3_Manager():
     def __init__(self):
         self.bucket_name = os.environ["s3_bucket"]
@@ -16,21 +67,30 @@ class S3_Manager():
 
     def rename_s3_directory_file(self, old_dir_name, new_dir_name, dir_path="/"):
         try:
-            # Copy object A as object B
-            copy_source = {'Bucket': self.bucket_name, 'Key': old_dir_name + dir_path}
-            s3_client.copy_object(Bucket=self.bucket_name, CopySource=copy_source, Key=new_dir_name + dir_path)
-            print("Copying old directory/file {} to new directory/file {}".format(old_dir_name + dir_path, new_dir_name + dir_path))
+            # Create a new directory
+            s3_client.put_object(Bucket=self.bucket_name, Key=(new_dir_name + dir_path))
 
-            # Delete the former object A
-            s3_client.delete_object(Bucket=self.bucket_name, Key=old_dir_name + dir_path)
-            print("Deleting old directory/file {}".format(old_dir_name + dir_path))
+            files = get_matching_s3_keys(bucket = self.bucket_name, prefix=old_dir_name + dir_path)
+            for each in files:
+                new_file = each.replace(old_dir_name + dir_path, new_dir_name + dir_path)
+                print("Copying directory/file {}".format(new_file))
+
+                copy_source = {'Bucket': self.bucket_name, 'Key': each}
+                s3_client.copy_object(CopySource = copy_source, Bucket = self.bucket_name, Key = new_file)
+
+                print("Deleting directory/file {}".format(old_dir_name + dir_path))
+                s3_client.delete_object(Bucket=self.bucket_name, Key=each)
+
         except Exception as error:
             print("Error occured while renaming directory/file from {} to {} due to {}".format(old_dir_name, new_dir_name, error))
 
     def delete_s3_directory_file(self, dir_name, dir_path="/"):
         try:
-            s3_client.delete_object(Bucket=self.bucket_name, Key=dir_name + dir_path)
-            print("Deleting directory/file {}".format(dir_name + dir_path))
+            files = get_matching_s3_keys(bucket = self.bucket_name, prefix=dir_name + dir_path)
+            for each in files:
+                print("Deleting directory/file {}".format(dir_name + dir_path))
+                s3_client.delete_object(Bucket=self.bucket_name, Key=each)
+
         except Exception as error:
             print("Error occured while deleting directory from {} due to {}".format(dir_name, error))
 
